53ee81c1b55f1e4020fc5b1743371e3670f906c5
Sven Malvik <sven.malvik@vipps.no>
2020-09-16 22:29:10 +0200

    .

--- src/main/java/com/svenmalvik/spaceship/MyController.java
+++ src/main/java/com/svenmalvik/spaceship/MyController.java
@@ -7,25 +7,36 @@ import org.springframework.ui.Model;
 import com.microsoft.azure.spring.cloud.feature.manager.*;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.beans.factory.annotation.Autowired;
+
 @Controller
 @ConfigurationProperties
-public class FeatureFlagController {
+public class MyController {
 
+    @Autowired
+    private MessageProperties properties;
     private FeatureManager featureManager;
 
-    public FeatureFlagController(FeatureManager featureManager) {
+    public MyController(FeatureManager featureManager) {
         this.featureManager = featureManager;
     }
 
     @GetMapping(value = {"", "/", "/welcome"})
     public String mainWithParam(Model model) {
-        model.addAttribute("Beta", featureManager.isEnabledAsync("featureManagement.beta").block());
+        model.addAttribute("beta", featureManager.isEnabledAsync("featureManagement.beta").block());
         return "welcome";
     }
 
     @GetMapping("/beta")
     public String mainWithParam2(Model model) {
-        model.addAttribute("Beta", featureManager.isEnabledAsync("featureManagement.beta").block());
+        model.addAttribute("message", properties.getMessage());
+        model.addAttribute("beta", featureManager.isEnabledAsync("featureManagement.beta").block());
         return "beta";
+    }
+
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        model.addAttribute("message", properties.getMessage());
     }
 }
